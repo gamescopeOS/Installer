5a8ac7529eba42f74790572e94c7ad8a8a61a070
Azel04 <75199595+Azel04@users.noreply.github.com>
2022-05-12 16:41:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 pacman -S gamescope
+echo "Moving to home directory..."
+cd ~
+echo "Moving to script directory..."
+cd Installer
+echo "Copying files..."
+cp xsession_files/*.* /usr/share/xsessions/ 
+echo "Done"
+
